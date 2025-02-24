7e4bded599bfac4dc4efb2292ca0cfb053e419ef
Thuc tap Sinh 1 <thuctapsinh1@mobifone.vn>
2025-02-24 15:34:40 +0700

    Map Jungle

---
+++ PRU_FinalProject-main/Assets/Script/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int HP = 100;
+    public GameObject ememy;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ememy = GameObject.Find("e");
+        // Khởi tạo hoặc kiểm tra gameObject đã được gán chưa
+    }
+
+    public virtual void Hp()
+    {
+        HP -= 5;
+        float at = 5f / 200f; // Sửa lại để phép chia chính xác
+
+        // Kiểm tra gameObject có được gán chưa
+        if (ememy != null)
+        {
+            // Điều chỉnh kích thước gameObject
+            ememy.transform.localScale = new Vector2(ememy.transform.localScale.x - at, ememy.transform.localScale.y);
+        }
+
+        Debug.Log(HP);
+    }
+}
---
+++ PRU_FinalProject-main/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    protected PlMove PlMove;
+    protected PlayerAttack PlayerAttack;
+    protected DamageReceived DamageReceived;
+    protected PlayerAnimation PlayerAnimation;
+    [SerializeField] private SpawnMagicSkill SpawnMagicSkill;
+
+    void Awake()
+    {
+        PlayerAttack = GetComponent<PlayerAttack>();
+        PlMove = GetComponent<PlMove>();
+        DamageReceived = GetComponent<DamageReceived>();
+        PlayerAnimation = GetComponent<PlayerAnimation>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            PlayerAnimation.NomalMagicSkill();
+            SpawnMagicSkill.Shoot(PlMove.CheckInput());
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("enemy"))
+        {
+            DamageReceived.TakeDamage(PlMove.CheckInput());
+        }
+    }
+
+    
+}
