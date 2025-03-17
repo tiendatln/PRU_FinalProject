b594a7015e074510ed5a43ee6204a34428954ce7
DatLNT <datle2212@gmail.com>
2025-03-17 23:31:42 +0700

    map5

--- Assets/Script/Map/Map_5/Enemy/Final Boss/Boss Attack MAp.cs
+++ Assets/Script/Map/Map_5/Enemy/Final Boss/Boss Attack MAp.cs
@@ -3,40 +3,15 @@ using UnityEngine.Tilemaps;
 
 public class BossAttackMAp : MonoBehaviour
 {
-    public Vector2 squareAttackSize;
-    public GameObject _attackPoint;
-    private Transform player;
-    private Vector2 AttackPoint;
-    public LayerMask layerMask;
-    private TilemapRenderer tilemapRenderer;
-    void Start()
-    {
-       
-        player = GameObject.FindGameObjectWithTag("Player").transform; // Tìm người chơi qua tag
-        tilemapRenderer = GetComponent<TilemapRenderer>();
+    public float _damage;
 
-        //squareAttackSize = tilemapRenderer.
-        AttackPoint = tilemapRenderer.transform.position;
-    }
 
-    
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(AttackPoint, squareAttackSize, layerMask);
-        foreach (Collider2D enemy in hitEnemies)
+        if (collision.gameObject.TryGetComponent<PlayerController>(out PlayerController player))
         {
-
-            // Kiểm tra xem enemy có component EnemyAI_2D hay không
-            if (enemy.gameObject.TryGetComponent<DamageReceived>(out DamageReceived player))
-            {
-                player.TakeDamage(30); // Gây sát thương lên kẻ địch
-            }
-
+            player.DamageReceived.TakeDamage(_damage); // Gây sát thương lên kẻ địch
         }
-    }
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireCube(AttackPoint, new Vector3(squareAttackSize.x, squareAttackSize.y, 0));
     }
 }
