6cae709f8f66aefc5cc85eac622a398108c412ab
Nhi <nhitrancaouyen@gmail.com>
2025-03-22 10:42:47 +0700

    map6 boss

---
+++ Assets/Script/Map/Map_6/BossMap6Animation.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class BossMap6Animation : MonoBehaviour
+{
+    public Animator animator;
+
+
+    [Header("Name Animation")]
+    private string _AttackName;
+    public string _Walk;
+    public string _Dead;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Walk()
+    {
+        animator.SetFloat(_Walk, 2f);
+    }
+
+    public void Dead()
+    {
+        animator.SetBool(_Dead, true);
+    }
+
+    public void Attack(string name)
+    {
+        animator.SetBool(name, true);
+        _AttackName = name;
+    }
+
+    public void StopAttack()
+    {
+        animator.SetBool(_AttackName, false);
+    }
+
+    public void Dash()
+    {
+        animator.SetBool("Dash", true);
+    }
+
+    public void StopDash()
+    {
+        animator.SetBool("Dash", false);
+    }
+
+    public void DrinkPotion()
+    {
+        animator.SetBool("DrinkPotion", true);
+    }
+
+    public void StopDrinkPotion()
+    {
+        animator.SetBool("DrinkPotion", false);
+    }
+}
