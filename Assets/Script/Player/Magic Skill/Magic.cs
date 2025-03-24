ae8693a326b92ead5dab1f79d5869d0c07d17020
unknown <dangvohong2k4@gmail.com>
2025-03-24 19:45:49 +0700

    Map4

--- Assets/Script/Player/Magic Skill/Magic.cs
+++ Assets/Script/Player/Magic Skill/Magic.cs
@@ -60,5 +60,10 @@ public class Magic : MonoBehaviour
             SandSwormAI.TakeDamage(playerController.PlayerMainData().attackSkill); // Map 2
             skill.SetActive(false);
         }
+        if (collision.gameObject.TryGetComponent<FinalBoss>(out FinalBoss FinalBoss))
+        {
+            FinalBoss.TakeDamage(playerController.PlayerMainData().attackSkill); // Map 2
+            skill.SetActive(false);
+        }
     }
 }
